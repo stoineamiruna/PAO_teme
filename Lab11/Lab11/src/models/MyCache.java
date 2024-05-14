a1858c553b889117b871f9852db95a290b8e9beb
Stoinea Maria-Miruna <130232381+stoineamiruna@users.noreply.github.com>
2024-05-14 17:49:43 +0300

    Update MyCache.java

--- Lab11/Lab11/src/models/MyCache.java
+++ Lab11/Lab11/src/models/MyCache.java
@@ -6,6 +6,7 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class MyCache extends Thread {
     private Map<Integer, StoredObject> cache = new ConcurrentHashMap<>();
+    private int cnt = 0;
 
     public void addStoredObject(int key, StoredObject storedObject) {
         cache.put(key, storedObject);
@@ -13,25 +14,29 @@ public class MyCache extends Thread {
 
     @Override
     public void run() {
-        while (true) {
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            Timestamp crt = new Timestamp(System.currentTimeMillis());
-            System.out.println("Cache size before expiration: " + cache.size());
-            for (Map.Entry<Integer, StoredObject> entry : cache.entrySet()) {
-                StoredObject storedObject = entry.getValue();
-                Timestamp expirationTime = storedObject.getExpirationTime();
-                if (expirationTime != null && crt.after(expirationTime)) {
-                    storedObject.setExpired(true);
-                    System.out.println("Expired object removed: " + entry.getKey());
+        try {
+            do{
+                for (Map.Entry<Integer, StoredObject> entry : cache.entrySet()) {
+                    if(cnt == cache.size())
+                        break;
+                    StoredObject storedObject = entry.getValue();
+                    if(!storedObject.isExpired()){
+                        Timestamp crt = new Timestamp(System.currentTimeMillis());
+                        Timestamp expirationTime = storedObject.getExpirationTime();
+                        if (expirationTime != null && crt.after(expirationTime)) {
+                            cnt++;
+                            storedObject.setExpired(true);
+                            System.out.println("Expired object removed: " + entry.getKey());
+                        }
+                    }
                 }
             }
-            System.out.println("Cache size after expiration: " + cache.size());
+            while(cnt != cache.size());
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+
     }
 
 
